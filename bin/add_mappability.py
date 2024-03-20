6ae94f7beb8ca4af14e95dfe0bbb24c268705dac
chelauk <chela.james@fht.org>
2024-03-20 11:22:49 +0100

    added mappabilty

--- bin/add_mappability.py
+++ bin/add_mappability.py
@@ -5,6 +5,7 @@
 import sys
 
 args = sys.argv
+print(args)
 
 location = {}
 with open(args[1], 'r') as f:
@@ -12,9 +13,16 @@ with open(args[1], 'r') as f:
     lines = [line.strip() for line in lines]
     lines = [line.split('\t') for line in lines]
     for line in lines:
-        location[line[0]] = line[4]
+        my_key = line[0].split("_")
+        my_key = [my_key[0], int(my_key[1])]
+        # the key is the location
+        my_key = tuple(my_key)
+        # the value is the mappability
+        location[my_key] = line[4]
 
 out_file = args[2].split('.')[0] + '.adj.maf'
+
+found_location = []
 with open(out_file, 'w') as f:
     with open(args[2], 'r') as g:
         for number,line in enumerate(g, start=1):
@@ -25,8 +33,13 @@ with open(out_file, 'w') as f:
                 f.write(line + '\t' + 'mappability' + '\n')
             if number > 2:
                 line_list = line.split('\t')
-                maf_loc = line_list[4] + '_' + line_list[5]
-                if maf_loc in location:
-                    f.write(line + '\t' + location[maf_loc] + '\n')
+                # this ugly if else series allows matching near the change
+                maf_loc = [line_list[4],line_list[5]]
+                if tuple([maf_loc[0],maf_loc[1]]) in location:
+                    f.write(line + '\t' + location[tuple([maf_loc[0],maf_loc[1]])] + '\n')
+                elif tuple([maf_loc[0],int(maf_loc[1]) + 1]) in location:
+                    f.write(line + '\t' + location[tuple([maf_loc[0],int(maf_loc[1]) + 1])] + '\n')
+                elif tuple([maf_loc[0],int(maf_loc[1]) - 1]) in location:
+                    f.write(line + '\t' + location[tuple([maf_loc[0],int(maf_loc[1]) - 1])] + '\n')
                 else:
                     f.write(line + '\t' + 'NA' + '\n')
